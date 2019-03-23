4052a5ac93bf7e5508b32dd93be1ca12ad310472
SteveLucasCS <stevelucasCS@gmail.com>
2019-03-23 05:44:57 -0500

    page now redirects to portfolio website

---
+++ README.md
@@ -0,0 +1,2 @@
+## My Portfolio can be found at: https://www.stevelucas.tech
+This page will automatically redirect to the link above.
--- assets/javascript/functionality.js
+++
@@ -1,5 +0,0 @@
-$(".link").on("click", function(event) {
-  event.preventDefault();
-  var source = event.target;
-  window.open($(source).data("link"),"_blank");
-})
